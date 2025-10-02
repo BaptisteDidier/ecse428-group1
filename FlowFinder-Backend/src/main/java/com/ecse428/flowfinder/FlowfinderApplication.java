320bbc960f07dc6fde42ea27922bfd85b8cff884
BaptisteDidier <baptiste.didier@yahoo.com>
2025-10-02 14:04:28 -0400

    Initial structure

---
+++ FlowFinder-Backend/src/main/java/com/ecse428/flowfinder/FlowfinderApplication.java
@@ -0,0 +1,13 @@
+package com.ecse428.flowfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowfinderApplication.class, args);
+	}
+
+}
---
+++ FlowFinder-Backend/src/test/java/com/ecse428/flowfinder/FlowfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecse428.flowfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
