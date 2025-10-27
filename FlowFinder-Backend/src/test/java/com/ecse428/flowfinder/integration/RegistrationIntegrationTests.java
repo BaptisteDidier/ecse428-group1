42e67db8040a58a96d8cdf049d45062be0c8639b
vivianeltain <vivianelaurat@gmail.com>
2025-10-26 23:48:32 -0400

    set up integration test for registration

--- FlowFinder-Backend/src/test/java/com/ecse428/flowfinder/integration/RegistrationIntegrationTests.java
+++ FlowFinder-Backend/src/test/java/com/ecse428/flowfinder/integration/RegistrationIntegrationTests.java
@@ -1,5 +1,32 @@
 package com.ecse428.flowfinder.integration;
 
 public class RegistrationIntegrationTests {
-    
+
+    @Autowired
+    private MockMvc MockMvc;
+
+    @Autowired
+    private RegistrationRepository registrationRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private InstrusctorRepository instructorRepository;
+
+    @Autowired
+    private DanceClassRepository danceClassRepository;
+
+    @Autowired
+    private SpecificClassRepository specificClassRepository;
+
+    @BeforeEach
+    public void setup() {
+        registrationRepository.deleteAll();
+        clientRepository.deleteAll();
+        instructorRepository.deleteAll();
+        danceClassRepository.deleteAll();
+        specificClassRepository.deleteAll();
+    }
+
 }
