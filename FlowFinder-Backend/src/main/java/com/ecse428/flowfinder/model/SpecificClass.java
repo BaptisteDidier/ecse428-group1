c19169bb2c97ac2510591661ce5c71a25ada91eb
vivianeltain <vivianelaurat@gmail.com>
2025-10-27 12:51:33 -0400

    added validation methods to SpecificClass

--- FlowFinder-Backend/src/main/java/com/ecse428/flowfinder/model/SpecificClass.java
+++ FlowFinder-Backend/src/main/java/com/ecse428/flowfinder/model/SpecificClass.java
@@ -23,7 +23,16 @@ public class SpecificClass {
   @ManyToOne
   private Instructor instructor;
 
-  public SpecificClass(boolean aIsDeleted, String aLocation, LocalDate aDate, int aLimit, LocalTime aStart, LocalTime aEnd, DanceClass aClass, Instructor aInstructor) {
+  public SpecificClass(boolean aIsDeleted, String aLocation, LocalDate aDate, int aLimit, LocalTime aStart,
+      LocalTime aEnd, DanceClass aClass, Instructor aInstructor) {
+    validateLimit(aLimit);
+    validateStartAndEndTime(aStart, aEnd);
+    validateDateAndTime(aDate, aStart, aEnd);
+
+    if (aClass == null || aInstructor == null) {
+      throw new IllegalArgumentException("DanceClass and Instructor must not be null");
+    }
+
     isDeleted = aIsDeleted;
     location = aLocation;
     date = aDate;
@@ -39,18 +48,22 @@ public class SpecificClass {
   }
 
   public void setDate(LocalDate aDate) {
+    validateDateAndTime(aDate, this.start, this.end);
     date = aDate;
   }
 
   public void setLimit(int aLimit) {
+    validateLimit(aLimit);
     limit = aLimit;
   }
 
   public void setStart(LocalTime aStart) {
+    validateDateAndTime(this.date, aStart, this.end);
     start = aStart;
   }
 
   public void setEnd(LocalTime aEnd) {
+    validateDateAndTime(this.date, this.start, aEnd);
     end = aEnd;
   }
 
@@ -108,16 +121,58 @@ public class SpecificClass {
     return wasSet;
   }
 
+  private void validateLimit(int aLimit) {
+    if (aLimit <= 0) {
+      throw new IllegalArgumentException("Limit must be positive");
+    }
+  }
+
+  private void validateDateAndTime(LocalDate date, LocalTime start, LocalTime end) {
+    LocalDate today = LocalDate.now();
+    LocalTime now = LocalTime.now();
+
+    if (date.isBefore(today)) {
+      throw new IllegalArgumentException("Date of class cannot be in the past");
+    }
+    validateStartAndEndTime(start, end);
+
+    if (date.isEqual(today)) {
+      if (!start.isAfter(now) || !end.isAfter(now)) {
+        throw new IllegalArgumentException("Class start and end time must be after current time");
+      }
+
+    }
+  }
+
+  public void validateStartAndEndTime(LocalTime start, LocalTime end) {
+    if (end.isBefore(start)) {
+      throw new IllegalArgumentException("End time is before start time");
+    }
+    if (end.equals(start)) {
+      throw new IllegalArgumentException("End time is equal to start time");
+    }
+  }
+
   public String toString() {
-    return super.toString() + "["+
-            "id" + ":" + getId()+ "," +
-            "isDeleted" + ":" + getIsDeleted()+ "," +
-            "location" + ":" + getLocation()+ "," +
-            "limit" + ":" + getLimit()+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "date" + "=" + (getDate() != null ? !getDate().equals(this)  ? getDate().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
-            "  " + "start" + "=" + (getStart() != null ? !getStart().equals(this)  ? getStart().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
-            "  " + "end" + "=" + (getEnd() != null ? !getEnd().equals(this)  ? getEnd().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
-            "  " + "class = "+(getClass()!=null?Integer.toHexString(System.identityHashCode(getClass())):"null") + System.getProperties().getProperty("line.separator") +
-            "  " + "instructor = "+(getInstructor()!=null?Integer.toHexString(System.identityHashCode(getInstructor())):"null");
+    return super.toString() + "[" +
+        "id" + ":" + getId() + "," +
+        "isDeleted" + ":" + getIsDeleted() + "," +
+        "location" + ":" + getLocation() + "," +
+        "limit" + ":" + getLimit() + "]" + System.getProperties().getProperty("line.separator") +
+        "  " + "date" + "="
+        + (getDate() != null ? !getDate().equals(this) ? getDate().toString().replaceAll("  ", "    ") : "this"
+            : "null")
+        + System.getProperties().getProperty("line.separator") +
+        "  " + "start" + "="
+        + (getStart() != null ? !getStart().equals(this) ? getStart().toString().replaceAll("  ", "    ") : "this"
+            : "null")
+        + System.getProperties().getProperty("line.separator") +
+        "  " + "end" + "="
+        + (getEnd() != null ? !getEnd().equals(this) ? getEnd().toString().replaceAll("  ", "    ") : "this" : "null")
+        + System.getProperties().getProperty("line.separator") +
+        "  " + "class = " + (getClass() != null ? Integer.toHexString(System.identityHashCode(getClass())) : "null")
+        + System.getProperties().getProperty("line.separator") +
+        "  " + "instructor = "
+        + (getInstructor() != null ? Integer.toHexString(System.identityHashCode(getInstructor())) : "null");
   }
 }
