a68a66dc462dd8a543d00b18687c3c88ac156010
vivianeltain <vivianelaurat@gmail.com>
2025-10-27 14:07:30 -0400

    moved functions added to Specific class previously to service instead and complete Specific Class service

--- FlowFinder-Backend/src/main/java/com/ecse428/flowfinder/model/SpecificClass.java
+++ FlowFinder-Backend/src/main/java/com/ecse428/flowfinder/model/SpecificClass.java
@@ -25,15 +25,6 @@ public class SpecificClass {
 
   public SpecificClass(boolean aIsDeleted, String aLocation, LocalDate aDate, int aLimit, LocalTime aStart,
       LocalTime aEnd, DanceClass aClass, Instructor aInstructor) {
-    validateLimit(aLimit);
-    validateStartAndEndTime(aStart, aEnd);
-    validateDateAndTime(aDate, aStart, aEnd);
-    validateLocation(aLocation);
-
-    if (aClass == null || aInstructor == null) {
-      throw new IllegalArgumentException("DanceClass and Instructor must not be null");
-    }
-
     isDeleted = aIsDeleted;
     location = aLocation;
     date = aDate;
@@ -45,27 +36,22 @@ public class SpecificClass {
   }
 
   public void setLocation(String aLocation) {
-    validateLocation(aLocation);
     location = aLocation;
   }
 
   public void setDate(LocalDate aDate) {
-    validateDateAndTime(aDate, this.start, this.end);
     date = aDate;
   }
 
   public void setLimit(int aLimit) {
-    validateLimit(aLimit);
     limit = aLimit;
   }
 
   public void setStart(LocalTime aStart) {
-    validateDateAndTime(this.date, aStart, this.end);
     start = aStart;
   }
 
   public void setEnd(LocalTime aEnd) {
-    validateDateAndTime(this.date, this.start, aEnd);
     end = aEnd;
   }
 
@@ -121,52 +107,6 @@ public class SpecificClass {
       wasSet = true;
     }
     return wasSet;
-  }
-
-  private void validateLimit(int aLimit) {
-    if (aLimit <= 0) {
-      throw new IllegalArgumentException("Limit must be positive");
-    }
-  }
-
-  private void validateDateAndTime(LocalDate date, LocalTime start, LocalTime end) {
-    LocalDate today = LocalDate.now();
-    LocalTime now = LocalTime.now();
-
-    if (date == null) {
-      throw new IllegalArgumentException("Date cannot be null");
-    }
-
-    if (date.isBefore(today)) {
-      throw new IllegalArgumentException("Date of class cannot be in the past");
-    }
-    validateStartAndEndTime(start, end);
-
-    if (date.isEqual(today)) {
-      if (!start.isAfter(now) || !end.isAfter(now)) {
-        throw new IllegalArgumentException("Class start and end time must be after current time");
-      }
-
-    }
-  }
-
-  public void validateStartAndEndTime(LocalTime start, LocalTime end) {
-    if (start == null || end == null) {
-      throw new IllegalArgumentException("Start and end times cannot be null");
-    }
-
-    if (!end.isAfter(start)) {
-      throw new IllegalArgumentException("End time cannot be equal or before start time");
-    }
-  }
-
-  public void validateLocation(String aLocation) {
-    if (aLocation == null) {
-      throw new IllegalArgumentException("Location cannot be null");
-    }
-    if (aLocation.trim().isEmpty()) {
-      throw new IllegalArgumentException("Location cannot be empty");
-    }
   }
 
   public String toString() {
