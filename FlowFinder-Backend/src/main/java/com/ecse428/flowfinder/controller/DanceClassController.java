b41055a4f7482b563c9096b0fdbc44a733ef2bf0
XanderG0 <xandergott03@gmail.com>
2025-10-28 07:49:17 -0400

    Add new class controller and DTOs implemented

--- FlowFinder-Backend/src/main/java/com/ecse428/flowfinder/controller/DanceClassController.java
+++ FlowFinder-Backend/src/main/java/com/ecse428/flowfinder/controller/DanceClassController.java
@@ -1,5 +1,31 @@
 package com.ecse428.flowfinder.controller;
 
+import com.ecse428.flowfinder.dto.CreateDanceClassRequest;
+import com.ecse428.flowfinder.dto.DanceClassResponse;
+import com.ecse428.flowfinder.service.DanceClassService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/dance-classes")
 public class DanceClassController {
     
-}
+    private final DanceClassService danceClassService;
+
+    public DanceClassController(DanceClassService danceClassService) {
+        this.danceClassService = danceClassService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public DanceClassResponse create(@Valid @RequestBody CreateDanceClassRequest request) {
+        return DanceClassResponse.from(danceClassService.createDanceClass(
+            request.getIsPrivate(),
+            request.getName(),
+            request.getGenre(),
+            request.getDescription()
+        ));
+    }
+} 
